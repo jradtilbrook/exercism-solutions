7f7573c392e1c07a76813f17ad0282bc9dad3f6a
jradtilbrook <jrad.tilbrook@gmail.com>
2018-10-31 15:05:07 +0800

    Use a more complex haskell concept

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -5,7 +5,7 @@ square n
     | n < 1 || n > 64 = Nothing
     | n == 1 = Just 1
     | n == 2 = Just 2
-    | otherwise = Just (2 ^ (n - 1))
+    | otherwise = (*) 2 <$> square (n - 1)
 
 total :: Integer
 total = 2 ^ 64 - 1
