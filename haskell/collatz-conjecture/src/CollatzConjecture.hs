ef82490bcad9252d12b8c126871301c8c2924c67
jradtilbrook <jrad.tilbrook@gmail.com>
2018-11-03 18:13:25 +0800

    Use approach that doesnt require two variables in recursive function

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,10 +2,9 @@ module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
 collatz n
-    | n >= 1 = Just (rec n 0)
-    | otherwise = Nothing
-
-rec n count
-    | n == 1 = count
-    | even n = rec (n `quot` 2) count + 1
-    | otherwise = rec (n * 3 + 1) count + 1
+    | n <= 0 = Nothing
+    | n == 1 = Just 0
+    | even n = nextStep $ n `div` 2
+    | otherwise = nextStep $ 3 * n + 1
+    where
+        nextStep = fmap (1 +) . collatz
