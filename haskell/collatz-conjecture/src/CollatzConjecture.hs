053f4548382e78db92ca188d9debe39cd1802a72
jradtilbrook <jrad.tilbrook@gmail.com>
2018-11-19 21:06:10 +0800

    Improve collatz solution

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -4,7 +4,5 @@ collatz :: Integer -> Maybe Integer
 collatz n
     | n <= 0 = Nothing
     | n == 1 = Just 0
-    | even n = nextStep $ n `div` 2
-    | otherwise = nextStep $ 3 * n + 1
-    where
-        nextStep = fmap (1 +) . collatz
+    | n `mod` 2 == 0 = fmap (+1) $ collatz (n `div` 2)
+    | otherwise = fmap (+1) $ collatz (n * 3 + 1)
