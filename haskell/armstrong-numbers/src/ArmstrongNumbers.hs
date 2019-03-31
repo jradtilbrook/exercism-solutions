9783e510f77ddc11808a8dbb9eb77a932d0da945
jradtilbrook <jrad.tilbrook@gmail.com>
2019-03-31 17:50:15 +0800

    Solve armstrong-numbers

--- haskell/armstrong-numbers/src/ArmstrongNumbers.hs
+++ haskell/armstrong-numbers/src/ArmstrongNumbers.hs
@@ -1,4 +1,12 @@
 module ArmstrongNumbers (armstrong) where
 
+import Data.List
+
 armstrong :: Integral a => a -> Bool
-armstrong = error "You need to implement this function."
+armstrong n = n == sum (map (\x -> x ^ length l) l)
+    where
+        l = unfoldr unfolder n
+
+unfolder :: Integral a => a -> Maybe (a, a)
+unfolder 0 = Nothing
+unfolder x = Just (x `mod` 10, x `div` 10)
