498e2efba192452c6f14e520a6462082c37f5c2e
jradtilbrook <jrad.tilbrook@gmail.com>
2019-04-01 18:21:12 +0800

    Use Data.Set for sum-of-multiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,10 +1,8 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import Data.List
+import qualified Data.Set as Set
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples [0] _ = 0
-sumOfMultiples factors limit = sum $ nub $ concatMap (takeWhile (limit >)) multiples
-    where
-        -- get a list of lists representing the multiples of all the given factors
-        multiples = map (\x -> [x, x + x..]) factors
+sumOfMultiples factors limit = sum . Set.unions . map multiplesOf $ factors
+  where multiplesOf 0 = Set.singleton 0
+        multiplesOf n = Set.fromList [0,n..limit-1]
